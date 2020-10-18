676a9c61275c1adb480150c8b365f8a8f20cce7b
huanggengzhong <915835528@qq.com>
2020-10-18 11:04:59 +0800

    完成column列表,上传

---
+++ src/components/ColumnList.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="row">
+    <div v-for="column in columnList" :key="column.id" class="col-4 mb-4">
+      <div class="card h-100 shadow-sm">
+        <div class="card-body text-center">
+          <img
+            :src="column.avatar"
+            :alt="column.title"
+            class="rounded-circle border border-light w-25 my-3"
+          />
+          <h5 class="card-title">{{column.title}}</h5>
+          <p class="card-text text-left">{{column.description}}</p>
+          <a href="#" class="btn btn-outline-primary">进入专栏</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent, PropType, computed } from "vue";
+export interface ColumnProps {
+  id: number;
+  title: string;
+  avatar?: string;
+  description: string;
+}
+export default defineComponent({
+  name: "ColumnList",
+  props: {
+    list: {
+      // type: Array as ColumnList[]//报错
+      type: Array as PropType<ColumnProps[]>, //将构造函数转为泛型类型推断的方法
+      required: true
+    }
+  },
+  setup(props) {
+    const columnList = computed(() => {
+      return props.list.map(column => {
+        if (!column.avatar) {
+          column.avatar = require("@/assets/column.jpg");
+        }
+        return column;
+      });
+    });
+
+    return {
+      columnList
+    };
+  }
+});
+</script>
